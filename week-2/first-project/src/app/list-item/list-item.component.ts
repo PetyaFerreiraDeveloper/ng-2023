0dda85c2a205ea82c73b104f750dfb646749dfd7
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-06-30 12:04:48 +0200

    lifecycle hooks OnInit, OnDestroy

--- week-2/first-project/src/app/list-item/list-item.component.ts
+++ week-2/first-project/src/app/list-item/list-item.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 
 export interface ICustomEvent {
   test: number;
@@ -9,16 +16,28 @@ export interface ICustomEvent {
   templateUrl: './list-item.component.html',
   styleUrls: ['./list-item.component.scss'],
 })
-export class ListItemComponent {
+export class ListItemComponent implements OnInit, OnDestroy {
   @Input() userObj!: { firstName: string; lastName: string };
   @Input() showLastName!: boolean;
   @Input() staticString!: string;
 
   @Output() customEvent = new EventEmitter<ICustomEvent>();
 
-  constructor() {}
+  intervalId: number | undefined;
+
+  constructor() {
+    console.log(this.userObj);
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+  ngOnInit() {
+    this.intervalId = setInterval(() => {}, 5000) as unknown as number;
+  }
+
   selectClickHandler($event: MouseEvent) {
-    $event.stopPropagation()
+    $event.stopPropagation();
     this.customEvent.emit({ test: 123 });
   }
 }
