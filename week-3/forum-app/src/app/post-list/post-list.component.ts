746e03c0bccf13c814fcd1d82e9db968c101ca8f
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-07-11 22:14:29 +0200

    render posts and themes

--- week-3/forum-app/src/app/post-list/post-list.component.ts
+++ week-3/forum-app/src/app/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { IPost } from '../interfaces/post';
 
 @Component({
   selector: 'app-post-list',
@@ -7,11 +8,19 @@ import { ApiService } from '../api.service';
   styleUrls: ['./post-list.component.scss'],
 })
 export class PostListComponent implements OnInit {
+  posts: IPost[] | null = null;
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.apiService.loadPosts(5).subscribe((value) => {
-      console.log('posts', value);
+    this.apiService.loadPosts(5).subscribe({
+      next: (value) => {
+        this.posts = value;
+        console.log('posts', value);
+      },
+      error: (err) => {
+        console.error(err);
+      },
     });
   }
 }
--- week-3/forum-app/src/app/theme-list/theme-list.component.ts
+++ week-3/forum-app/src/app/theme-list/theme-list.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { ITheme } from '../interfaces/theme';
 
 @Component({
   selector: 'app-theme-list',
   templateUrl: './theme-list.component.html',
-  styleUrls: ['./theme-list.component.scss']
+  styleUrls: ['./theme-list.component.scss'],
 })
 export class ThemeListComponent implements OnInit {
-constructor(private apiService:ApiService) {
- 
-}
+  themeList: ITheme[] | null = null;
+  constructor(private apiService: ApiService) {}
 
-ngOnInit(): void {
-  this.apiService.loadThemes().subscribe((value) => {
-    console.log(value);
-    
-  })
-}
+  ngOnInit(): void {
+    this.apiService.loadThemes().subscribe({
+      next: (value) => {
+        this.themeList = value;
+        console.log(value);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
 }
