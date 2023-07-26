0cc943f6a89493cd3858434db4ac05f399963f63
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-07-26 11:01:35 +0200

    create structural directive

--- week-5/forms/src/app/my-router-link.directive.ts
+++ week-5/forms/src/app/my-router-link.directive.ts
@@ -1,12 +1,30 @@
-import { Directive, ElementRef, OnDestroy, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Directive({
   selector: '[appMyRouterLink]',
 })
-export class MyRouterLinkDirective implements OnDestroy {
+export class MyRouterLinkDirective implements OnInit, OnDestroy {
+  @Input() appMyRouterLink!: string;
+  @Input() template!: TemplateRef<any>;
+
   unsubs: (() => void)[] = [];
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+  viewHasBeenCreated = false;
+
+  constructor(
+    private elementRef: ElementRef,
+    private vc: ViewContainerRef,
+    private renderer: Renderer2
+  ) {
     console.log(elementRef);
     this.unsubs.push(
       this.renderer.listen(
@@ -29,13 +47,22 @@ export class MyRouterLinkDirective implements OnDestroy {
     );
   }
 
+  ngOnInit(): void {
+    console.log(this.appMyRouterLink, this.template);
+  }
+
   mouseOverHandler = (e: MouseEvent) => {
-    console.log('clicked');
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'red');
+    if (this.viewHasBeenCreated) return;
+    this.vc.createEmbeddedView(this.template);
+    this.viewHasBeenCreated = true;
   };
 
   mouseLeaveHandler = (e: MouseEvent) => {
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'initial');
+    this.vc.clear()
+    this.viewHasBeenCreated = false;
+
   };
 
   ngOnDestroy(): void {
