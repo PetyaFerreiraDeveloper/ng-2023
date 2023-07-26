e8d908aba1560e2270fcc418389e6485118f3374
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-07-26 10:45:14 +0200

    create an attribute directive

--- week-5/forms/src/app/my-router-link.directive.ts
+++ week-5/forms/src/app/my-router-link.directive.ts
@@ -1,13 +1,44 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, OnDestroy, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appMyRouterLink]'
+  selector: '[appMyRouterLink]',
 })
-export class MyRouterLinkDirective {
+export class MyRouterLinkDirective implements OnDestroy {
+  unsubs: (() => void)[] = [];
 
-  constructor(private elementRef: ElementRef) { 
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
     console.log(elementRef);
-    
+    this.unsubs.push(
+      this.renderer.listen(
+        this.elementRef.nativeElement,
+        'mouseover',
+        this.mouseOverHandler
+      )
+    );
+    this.unsubs.push(
+      this.renderer.listen(
+        this.elementRef.nativeElement,
+        'mouseleave',
+        this.mouseLeaveHandler
+      )
+    );
+    this.renderer.setAttribute(
+      this.elementRef.nativeElement,
+      'data-test',
+      '123'
+    );
   }
 
+  mouseOverHandler = (e: MouseEvent) => {
+    console.log('clicked');
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'red');
+  };
+
+  mouseLeaveHandler = (e: MouseEvent) => {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'initial');
+  };
+
+  ngOnDestroy(): void {
+    this.unsubs.forEach((fn) => fn());
+  }
 }
