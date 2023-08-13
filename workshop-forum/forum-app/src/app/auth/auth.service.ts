d02cb81d9d3fad509ac0ffac2399bd7c0af1cf8b
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-08-13 11:53:26 +0200

    start user profile

--- workshop-forum/forum-app/src/app/auth/auth.service.ts
+++ workshop-forum/forum-app/src/app/auth/auth.service.ts
@@ -5,7 +5,18 @@ import { IUser } from '../shared/interfaces';
   providedIn: 'root',
 })
 export class AuthService {
-  user: IUser | null = null;
+  user: IUser | null = {
+    themes: ['hello', 'mello'],
+    posts: ['post1', 'post2'],
+    _id: 'someId',
+    tel: '123456',
+    email: 'misho@abv.bg',
+    username: 'misho',
+    password: 'mishosPass',
+    created_at: '12 july 2020',
+    updatedAt: '12 july 2020',
+    __v: 1,
+  };
 
   get isLoggedIn() {
     return this.user !== null;
--- workshop-forum/forum-app/src/app/auth/profile/profile.component.ts
+++ workshop-forum/forum-app/src/app/auth/profile/profile.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { appEmailDomains } from 'src/app/shared/constants';
+import { appEmailValidator } from 'src/app/shared/validators';
+import { AuthService } from '../auth.service';
+import { IUser } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
+  showEditMode = false;
 
+  get user() {
+    const { username, email, tel: telephone } = this.authService.user!;
+    const [code, ...tel] = telephone.split(' ');
+    return {
+      username,
+      email,
+      tel: tel.join(' '),
+      code,
+    };
+  }
+
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, appEmailValidator(appEmailDomains)]],
+    tel: [''],
+    code: [''],
+  });
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
+
+  toggleEditMode(): void{ 
+    this.showEditMode = !this.showEditMode
+    console.log(this.showEditMode);
+    
+  } 
 }
