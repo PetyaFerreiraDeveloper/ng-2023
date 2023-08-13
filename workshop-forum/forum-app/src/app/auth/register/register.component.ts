14ae344f730ff4b13c958bcc92bd4621bb9a9259
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-08-13 11:12:52 +0200

    add register handler

--- workshop-forum/forum-app/src/app/auth/register/register.component.ts
+++ workshop-forum/forum-app/src/app/auth/register/register.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { catchError, map, of } from 'rxjs';
 import { appEmailDomains } from 'src/app/shared/constants';
-import { appEmailValidator, sameValueGroupValidator } from 'src/app/shared/validators';
+import {
+  appEmailValidator,
+  sameValueGroupValidator,
+} from 'src/app/shared/validators';
 
 @Component({
   selector: 'app-register',
@@ -10,8 +14,8 @@ import { appEmailValidator, sameValueGroupValidator } from 'src/app/shared/valid
 })
 export class RegisterComponent {
   form = this.fb.group({
-    username: ['', [Validators.required]],
-    email: ['', [Validators. required, appEmailValidator(appEmailDomains)]],
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, appEmailValidator(appEmailDomains)]],
     code: [''],
     tel: [''],
     pass: this.fb.group(
@@ -26,4 +30,24 @@ export class RegisterComponent {
   });
 
   constructor(private fb: FormBuilder) {}
+
+  registerHandler() {
+    if (this.form.invalid) return;
+    console.log(this.form.value);
+
+    this.form.reset();
+  }
 }
+
+// of(1).pipe(
+//   map(() => {
+//     throw new Error('BAD ERROR')
+//   }),
+//   catchError(error => {
+//     console.log(error)
+//     return [1000]
+//   })
+// ).subscribe({
+//   next: (value) => {console.log(value)},
+//   error: (error) => console.log(error)
+// })
